a6b3f7fa16d6dfbe91800032b2c0a7b7e4d9c5f3
Fernando Antonio Melara Calix <fmelara750@gmail.com>
2024-08-13 19:59:56 -0600

    Creacion de index.php co  estructura html

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Fernando Antonio Melara Calix</title>
+        <link rel="stylesheet" href="css/bootstrap.min.css"/>
+        <link rel="stylesheet" href="css/style.css"/>
+        <scrip src="js/jqueri-3.7.1.min.js"></scrip>
+        <scrip src="js/bootstrap.bundle.min.js"></scrip>
+    </head>
+    <body>
+
+    </body>
+</html>
